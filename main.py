7b21a81fa16e5ba2bb4fc713db548f53914fa2f0
InvarGervi123 <InvarGervi1234@gmail.com>
2026-02-18 12:25:38 +0200

    2

--- main.py
+++ main.py
@@ -6,3 +6,19 @@ df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
 df['target'] = iris.target
 
 print(df.head())
+
+from sklearn.model_selection import train_test_split
+
+X = df[iris.feature_names]
+y = df['target']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+
+from sklearn.neighbors import KNeighborsClassifier
+
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train,y_train)
+
+accuracy = model.score(X_test,y_test)
+print(f"Model accuracy: {accuracy * 100:.2f}%")
